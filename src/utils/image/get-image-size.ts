870637397889c4e4592e8927ade96e44c8edbf7f
william <williamkotronakis@gmail.com>
2024-03-10 20:26:38 +0200

    fix: handle svg images without natural width and/or height on firefox

--- src/utils/image/get-image-size.ts
+++ src/utils/image/get-image-size.ts
@@ -5,6 +5,20 @@ export async function getImageSize(blob: Blob): Promise<{
   return new Promise((resolve, reject) => {
     const img = new Image();
     img.onload = function () {
+      if (navigator.userAgent.toLowerCase().includes("firefox") && blob.type === "image/svg+xml") {
+        // mimicking chrome user agent behavior
+        if (!img.naturalWidth && !img.naturalHeight) {
+          img.style.maxWidth = "150px";
+        } else if (!img.naturalWidth) {
+          img.style.width = `${img.naturalHeight}px}`
+        } else if (!img.naturalHeight) {
+          img.style.height = `${img.naturalWidth}px}`
+        }
+
+        // This is needed for all the above cases
+        document.body.append(img);
+      }
+
       resolve({
         height: img.height,
         width: img.width,
